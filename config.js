8567c655691e1fe5e6be095d96cf6bbac6001502
endo <endo.fablab@gmail.com>
2026-05-19 15:12:30 -0300

    Fix: actualizar token en config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const GITHUB_TOKEN = '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';
+const GITHUB_TOKEN = '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';
 const GITHUB_OWNER = 'endo-estudio';
 const GITHUB_REPO = 'endo-fl-demo';
